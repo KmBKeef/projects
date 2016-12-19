525251ff5c9c1a84016a15a804a91744c1cbc386
KmBKeef <mo.kefah@protonmail.com>
2016-12-19 14:19:59 +0200

    bubble sort

--- bubble.cpp
+++ bubble.cpp
@@ -1,55 +1,29 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main() {
-	
-	int no[] = {10 , 4, 5, 3, 2, 1};
-	
-	int* s1 = no;
-	int* s2 = no + 1;
-	int save = *s2;
-	bool swap = true;	
-	
-	
-	for(int x = 1; x < sizeof(no)/sizeof(int); x++) {
-		
-			s1 = no;
-			s2 = no +1;
-			save = *s2;
-		
-		for(int x = 2; x < sizeof(no)/sizeof(int); x++) {
 
-			if(*s1 > *s2){
-			*s2 = *s1;
-			*s1 = save;
+	int no[] = {10 , 9,8,7,6 ,5,4, 3,2, 1};
+	int count = sizeof(no)/sizeof(int);	
+	int tries = 0;
+
+
+
+	for(int x = 0; x < (count-1) ; x++) {
+		for(int y = 0; y < (count-1-x) ; y++) {
+			if(no[y] > no[y+1]) {
+				int z = no[y+1];
+				no[y+1] = no[y];
+				no[y] = z;
 			}
-			
-			s1++;
-			s2++;
-			save =*s2;
-			
-					
+			tries++;
 		}	
 		
-		if(*s1 > *s2) {
-				*s2 = *s1;
-				*s1 = save;
-			}	
+		for(int x = 0; x < count; x++) cout << no[x]<< " " << flush;
+ 	cout <<	endl;
 	}
-
-
-
-
-		for(int x = 0; x < sizeof(no)/sizeof(int); x++) {
-			
-			cout << no[x]<< " " << flush;
-				
-		}
-		
-		cout << endl;
-		cout << *s1 << " " << *s2 << endl;	
 	
+	
+	cout << tries << endl;
 	return 0;
 }
--- newex.cpp
+++ newex.cpp
@@ -13,7 +13,7 @@ int main() {
 //	b = b0 + b1 = q*m + n*p;
 //  b0 * b1 = a * c
 
-	cout << "Enter your falues for a, b and c: " << endl;
+	cout << "Enter your values for a, b and c: " << endl;
 	cout << "a: " << flush;
 	cin >> a;
 	
@@ -24,22 +24,22 @@ int main() {
 	cin >> c;
 
 
-  cout << "Input equation is " << a << "*x^2 + " << b << "*x + "<< c << endl;
+cout << "Input equation is " << "[" << a << "*x^2 + " << b << "*x + "<< c << "]" << endl << "==========================="<< endl;
   int count = 0;
   bool solution_found = false;
   for (n=-1*abs(c); n <= abs(c); n++) {
 			for (q=-1*abs(a); q <= abs(a); q++) {
           count++;
 					if (q != 0 && n != 0 && q*q*c + n*n*a == b*n*q) {
-						cout << "The solution is: (" <<q<<"*x + " << n << ")*(" << a/q<< "*x + " << c/n << ")" << endl; 
+						cout << "A possible solution is: (" <<q<<"*x + " << n << ")*(" << a/q<< "*x + " << c/n << ")\n" << endl; 
 						solution_found = true;
 					}
 			}
   }	
 
   if(! solution_found) {
-		cout << "Sorry, no solution was found" << endl;
+		cout << "Sorry, no solution was found\n" << endl;
   }
-	cout << "Took "<< count <<  " iterations" << endl;
+	cout << "===============================" << endl << "Took "<< count <<  " iterations" << endl;
 	return 0;
 }

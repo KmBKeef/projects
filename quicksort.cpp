4c3fbe6e6c476004e6884b8aad72fe2f9056610d
KmBKeef <mo.kefah@protonmail.com>
2016-12-29 14:22:17 +0200

    Done.

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,50 +1,49 @@
 #include <iostream>
 using namespace std;
 
+
+int count;
+
+void quickSort(int arr[], int left, int right) {
+		int b = left, e = right; int tmp; int pivot = (arr[(left + right) / 2]);
+		
+		while(b <= e) {
+			count++;
+			while (arr[b] < pivot)
+				b++;
+
+			while (arr[e] > pivot)
+				e--;
+
+			if (b <= e) {
+				tmp = arr[b]; 
+				arr[b] = arr[e]; 
+				arr[e] = tmp; 
+				b++; 
+				e--; 
+			} 
+		};
+
+		if (left < e) quickSort(arr, left, e);
+
+		if (b < right) quickSort(arr, b, right); 
+}
+	
+
 int main() {
+	int myarr [] = {5,4,3,2,1};
+	int noValues = sizeof(myarr)/sizeof(int);
+	for(int x = 0; x < noValues; x++)
+	 	cout << myarr[x]  << " " << flush;
+	cout << endl;
+	quickSort(myarr, 0, noValues-1)  ;
 
-	int arr[] = {21, 16, 23, 33, 10, 17, 20, 12, 3, 29};
-	int n = sizeof(arr)/sizeof(int);
-	int pivot;
-	int save;
-	int count = 0;
-	if(n % 2 == 0) {
-		pivot = ((n+2)/2)-1;
-	}
-	else {
-		pivot = ((n+1)/2)-1;
-	}
-
-	for(int x = pivot - 1; x >= 0; x--) {
-		if(arr[x] > arr[pivot]) {
-			save = arr[x];
-			for(int y = x; y <= pivot; y++)
-				arr[y] = arr[y+1];
-			arr[pivot] = save;
-			//x = pivot-1;
-			pivot--;
-		}
-		count++;
-	}
-
-	for(int h = pivot+1; h < n; h++) {
-		if(arr[h] < arr[pivot] && h > pivot) { // moves to the left
-			save = arr[h];
-			for(int z = h ; z >= pivot; z--)
-				arr[z] = arr[z-1];
-			arr[pivot] = save;
-			//h = pivot+1;
-			pivot++;
-		}
-		count++;
-	}
-
-	for(int w = 0; w < n; w++){cout << arr[w] << " " << flush;}cout << endl;	
-
-	//	cout << save;
-	//	cout << n << endl;
+	for(int x = 0; x < noValues; x++)
+	 	cout << myarr[x]  << " " << flush;
+	cout << endl;
 	cout << count << endl;
-	cout << pivot << endl;
-	cout << arr[pivot] << endl;
 	return 0;
 }
+
+//Easy peasy lemon sqeez
+ 
